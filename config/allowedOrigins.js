271b1db5755a0094eda52e236a23a636f7044932
SirMrPeter <piotr.ambroziak@4mycreation.com>
2023-03-22 10:41:46 +0100

    upload

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,11 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://127.0.0.1:3000',
     'http://localhost:3500',
+    'http://127.0.0.1:80',
+    'http://localhost:80',
+    'http://127.0.0.1:443',
+    'http://localhost:443',
     'http://localhost:3000',
     'http://localhost:4000',
     'http://192.168.0.74:3000',
